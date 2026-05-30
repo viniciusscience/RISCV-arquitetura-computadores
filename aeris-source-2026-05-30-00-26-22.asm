060f5e48dddde3a515bd6fac1c0a6e80e32cc089
Jose Vinicius <daftwarz@gmail.com>
2026-05-29 21:29:00 -0300

    arquitetura risc-v

---
+++ aeris-source-2026-05-30-00-26-22.asm
@@ -0,0 +1,33 @@
+.data
+VETOR_A: .word 3,5,9,11,13,15,17,19
+VETOR_B: .word 3,5,9,11,14,17,19,30
+
+.text
+main:
+    la t5 VETOR_A
+    la t6 VETOR_B
+    li s1,0
+    li s6,0
+    addi s0, zero, 0 
+
+Loop:
+    slti t0, s0, 8     
+    beq  t0, zero, Exit 
+    addi s0, s0, 1
+    slli a1,s1,2
+    add  t3, t5, a1
+    add  t4, t6, a1 
+    lw a5 0(t3)
+    lw a6 0(t4)
+    beq a5, a6 SOMA
+    j PROXIMO  
+
+PROXIMO:
+    addi s1, s1, 1
+    j Loop  
+
+SOMA:
+    addi s6,s6,1
+    j    Loop  
+Exit:
+    nop                  
